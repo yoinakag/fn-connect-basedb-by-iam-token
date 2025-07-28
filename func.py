9c539ed69b25e33a6d7c42731df331a9ef63c15b
yanlinfeng <yan.linfeng@foxmail.com>
2025-07-28 21:57:04 +0800

    update

--- func.py
+++ func.py
@@ -7,6 +7,22 @@ from fdk import response
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
+
+
+# Get connection parameters from enviroment
+basedb_region = os.getenv("BASEDB_REGION")
+basedb_compartment_ocid = os.getenv("BASEDB_COMPARTMENT_OCID")
+basedb_ocid = os.getenv("BASEDB_OCID")
+
+scope = "urn:oracle:db::id::{}::{}".format(basedb_region,basedb_compartment_ocid,basedb_ocid)
+oracledb.init_oracle_client(lib_dir="/usr/lib/oracle/23/client64/lib", config_dir="/tmp/instant23ai")
+
+#
+# Function Handler: executed every time the function is invoked
+#
+def handler(ctx, data: io.BytesIO = None):
+    return read_all_users(ctx)
+
 def _get_key_pair():
     """
     Generates a public-private key pair for proof of possession.
@@ -74,46 +90,19 @@ def _generate_access_token(client, token_auth_config):
 
     return (response.data.token, key_pair["private_key"])
 
-print("init")
-# Get connection parameters from enviroment
-basedb_region = os.getenv("BASEDB_REGION")
-basedb_compartment_ocid = os.getenv("BASEDB_COMPARTMENT_OCID")
-basedb_ocid = os.getenv("BASEDB_OCID")
-print("get parameter")
-
-scope = "urn:oracle:db::id::{}::{}".format(basedb_region,basedb_compartment_ocid,basedb_ocid)
-oracledb.init_oracle_client(lib_dir="/usr/lib/oracle/23/client64/lib",config_dir="/tmp/instant23ai")
-print("init oracle client")
-
-# signer = oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
-signer = oci.auth.signers.get_resource_principals_signer()
-client = oci.identity_data_plane.DataplaneClient(config={}, signer=signer)
-token_auth_config = {
-    "scope":"urn:oracle:db::id::ocid1.compartment.oc1..aaaaaaaardb3dtrfgv5dde2rqisd44p3f6ihjtbd3gnbtwq64nq6lzngxotq::ocid1.dbsystem.oc1.iad.anuwcljsak7gbriafwglhghmau64pqtimdyzqhkoryf7shdzs5ehuzo6t6sa",
-    "region":"us-ashburn-1"
-}
-print("get client")
-
-# connection = oracledb.connect(
-#     access_token=_generate_access_token(client, token_auth_config),
-#     dsn="iam",
-#     externalauth=True
-# )
-# print(connection)
-
-#
-# Function Handler: executed every time the function is invoked
-#
-def handler(ctx, data: io.BytesIO = None):
-    print("handle")
-    return read_all_users(ctx)
-
 def read_all_users(ctx):
     try:
         sql_statement = """
             SELECT 1+1
             FROM dual
         """
+        
+        client = oci.identity_data_plane.DataplaneClient(config={}, signer=oci.auth.signers.get_resource_principals_signer())
+        token_auth_config = {
+            "scope":"urn:oracle:db::id::ocid1.compartment.oc1..aaaaaaaardb3dtrfgv5dde2rqisd44p3f6ihjtbd3gnbtwq64nq6lzngxotq::ocid1.dbsystem.oc1.iad.anuwcljsak7gbriafwglhghmau64pqtimdyzqhkoryf7shdzs5ehuzo6t6sa",
+            "region":"us-ashburn-1"
+        }
+        
         with oracledb.connect(
             access_token=_generate_access_token(client, token_auth_config),
             dsn="iam",
