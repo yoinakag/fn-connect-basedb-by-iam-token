3acc0b329402b0ae195f0f3bc1464b52e236c710
yoinakag <49713638+yoinakag@users.noreply.github.com>
2025-10-02 14:22:58 +0900

    Update func.py

--- func.py
+++ func.py
@@ -173,9 +173,9 @@ restore_files_from_string(combined_str=wallet_base64)
 # Setup tnsnames.ora and sqlnet.ora file
 with open('/function/instant23ai/tnsnames.ora') as orig_tnsnamesora:
     newText=orig_tnsnamesora.read()
+    newText=newText.replace('HOST_PLACEHOLDER', host).replace('SERVICE_NAME_PLACEHOLDER',service_name).replace('CN=CN_PLACEHOLDER', cn)
     print("===== newText =====")
     print(newText)
-    newText=newText.replace('HOST_PLACEHOLDER', host).replace('SERVICE_NAME_PLACEHOLDER',service_name).replace('CN=CN_PLACEHOLDER', cn)
 with open('/tmp/dbwallet/tnsnames.ora', "w") as new_tnsnamesora:
     new_tnsnamesora.write(newText)
 with open('/function/instant23ai/sqlnet.ora') as sqlnetora:
